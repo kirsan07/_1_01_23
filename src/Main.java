19134a04736f96c56ba52ddd43b64dbe0f4caa0e
kirsan07 <terkirsun@gmail.com>
2023-01-09 17:26:41 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int input = scanner.nextInt();
+        i == 0;
+        while  (i<input){
+            int t = scanner.nextInt();
+            i++;
+        }
+        int s = scanner.nextInt();
+        c == 0;
+        while (true){
+            if (t>s){
+                c=c+t;
+            }
+        }
+        break;
+        System.out.println(c);
+    }
+}
