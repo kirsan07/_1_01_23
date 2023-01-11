85e2168d138c0f72b8b6b8ec38e6b7accb258418
t0in4 <t0in4@yahoo.com>
2023-01-11 09:38:42 -0500

    Update Main.java
    Немного исправил)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
-        i == 0;
-        while  (i<input){
-            int t = scanner.nextInt();
+        int i = 0;
+        // создали массив t размером input
+        int[] t = new int[input];
+        while (i < input) {
+            // каждому элементу массива присваиваем ввод числа с клавиатуры
+            t[i] = scanner.nextInt();
             i++;
         }
         int s = scanner.nextInt();
-        c == 0;
-        while (true){
-            if (t>s){
-                c=c+t;
+        // создали счетчик c для подсчета элементов массива t подходящих под условие
+        int c = 0;
+        // в рамках предложенной модели программы, нам нужна ещё одна переменная счетчик
+        // однако, просто обнулим ранее задекларированную переменную i
+        i = 0;
+        while (true) {
+            // условие для выхода из цикла
+            if (i == t.length) {
+                break;
             }
+            if (t[i] > s) {
+                c = c + t[i];
+            }
+
+            // увеличиваем каждую итерацию i на 1, т.к. i нам нужен для вызова
+            // элементов ранее определенного массива t
+            i++;
         }
-        break;
+
         System.out.println(c);
     }
 }
